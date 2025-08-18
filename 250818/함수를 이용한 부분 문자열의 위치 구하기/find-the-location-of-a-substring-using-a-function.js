b30fdf96d21fa69eb068a2cf3a4238d6728af46e
SK <54721624+ygvbhy@users.noreply.github.com>
2025-08-18 21:40:41 +0900

    Title: 함수를 이용한 부분 문자열의 위치 구하기, Time: 194ms, Memory: 15MB, Status: Passed - Codetree

---
+++ 250818/함수를 이용한 부분 문자열의 위치 구하기/find-the-location-of-a-substring-using-a-function.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split("\n");
+
+const text = input[0];
+const pattern = input[1];
+
+// Please Write your code here.
+console.log(text.indexOf(pattern))
