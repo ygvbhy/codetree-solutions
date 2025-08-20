34621d943c99fc3d245e0ba82e9a34ff6c56b8da
SK <54721624+ygvbhy@users.noreply.github.com>
2025-08-20 20:28:15 +0900

    Title: 1이 되는 순간까지, Time: 158ms, Memory: 15MB, Status: Wrong Answer - Codetree

---
+++ 250820/1이 되는 순간까지/until-the-moment-I-reach-one.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const n = Number(input[0]);
+// Please Write your code here.
+let cnt = 0;
+const f = (n) => {
+    if(n === 1 || n < 1) return;
+
+    cnt++
+    if (n % 2 === 0) f(n / 2)
+    else f(n / 3)
+}
+
+f(n)
+console.log(cnt)
