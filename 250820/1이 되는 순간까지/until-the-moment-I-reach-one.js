9f78673ffcd7115f84989c0a83a328944d3b3779
SK <54721624+ygvbhy@users.noreply.github.com>
2025-08-20 20:30:30 +0900

    Title: 1이 되는 순간까지, Time: 168ms, Memory: 15MB, Status: Passed - Codetree

--- 250820/1이 되는 순간까지/until-the-moment-I-reach-one.js
+++ 250820/1이 되는 순간까지/until-the-moment-I-reach-one.js
@@ -4,11 +4,11 @@ const n = Number(input[0]);
 // Please Write your code here.
 let cnt = 0;
 const f = (n) => {
-    if(n === 1 || n < 1) return;
+    if(n === 1) return;
 
     cnt++
     if (n % 2 === 0) f(n / 2)
-    else f(n / 3)
+    else f(parseInt(n / 3))
 }
 
 f(n)
