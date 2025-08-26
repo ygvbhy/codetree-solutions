bc2e0b9121e2ec5cf8dd4322495aa7b9f4ffb6cf
SK <54721624+ygvbhy@users.noreply.github.com>
2025-08-26 19:27:56 +0900

    Title: 단어 정렬, Time: 180ms, Memory: 15MB, Status: Passed - Codetree

---
+++ 250826/단어 정렬/sorting-words.js
@@ -0,0 +1,6 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const n = Number(input[0]);
+const words = input.slice(1, n + 1);
+// Please Write your code here.
+words.sort().map(s => console.log(s))
