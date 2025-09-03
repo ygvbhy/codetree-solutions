89ceac05d231431247c2af13f3648c8ec8311d1f
SK <54721624+ygvbhy@users.noreply.github.com>
2025-09-03 10:47:28 +0900

    Title: Date to Date, Time: 67ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250903/Date to Date/date-to-date.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const [m1, d1, m2, d2] = input[0].split(' ').map(Number);
+
+// Please Write your code here.
+const num_of_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+function dayOfYear(m, d) {
+    let sum = 0;
+    for (let i = 0; i < m - 1; i++) sum += num_of_days[i]
+    return sum + d
+}
+
+const a = dayOfYear(m1, d1)
+const b = dayOfYear(m2, d2)
+console.log(b - a)
