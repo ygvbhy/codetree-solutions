d989dedb04f06441b698208ffb080809883b635b
SK <54721624+ygvbhy@users.noreply.github.com>
2025-09-03 11:01:25 +0900

    Title: DateTime to DateTime, Time: 72ms, Memory: 10MB, Status: Passed - Codetree

--- 250903/DateTime to DateTime/datetime-to-datetime.js
+++ 250903/DateTime to DateTime/datetime-to-datetime.js
@@ -4,6 +4,9 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 const [A, B, C] = input[0].split(' ').map(Number);
 
 // Please write your code here.
+
+if (A === 11 && (B < 11 || C < 11)) return console.log(-1)
+
 let a = (A - 11) * 24 * 60
 let b = (B - 11) * 60
 let c = C - 11
