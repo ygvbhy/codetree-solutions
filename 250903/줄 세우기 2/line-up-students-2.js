ec5918de00bfd2907773e53bf245384369a0035b
SK <54721624+ygvbhy@users.noreply.github.com>
2025-09-03 10:05:53 +0900

    Title: 줄 세우기 2, Time: 69ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250903/줄 세우기 2/line-up-students-2.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+const studentInputs = [];
+for (let i = 1; i <= n; i++) {
+    const [height, weight] = input[i].split(' ').map(Number);
+    studentInputs.push([height, weight, i]);
+}
+
+// Please Write your code here.
+studentInputs.sort((a, b) => {
+    if (a[0] !== b[0]) return a[0] - b[0]
+    return b[1] - a[1]
+})
+studentInputs.map(n => console.log(n.join(" ")))
