8dd0dbac7245a8b96918c99da0bf505977b6d4f6
SK <54721624+ygvbhy@users.noreply.github.com>
2025-09-05 15:39:56 +0900

    Title: 여러가지 진수변환, Time: 63ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250905/여러가지 진수변환/various-numeral-system-transformations.js
@@ -0,0 +1,6 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+let [n, b] = input[0].split(' ').map(Number);
+
+// Please Write your code here.
+console.log(n.toString(b))
