f62a3eb1a53e98fe8ca50e03fd3f0fe537254c3d
SK <54721624+ygvbhy@users.noreply.github.com>
2025-09-09 23:40:53 +0900

    Title: 연속되는 수 2, Time: 64ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250909/연속되는 수 2/continuous-number2.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+const arr = input.slice(1, n + 1).map(Number);
+
+// Please Write your code here.
+let max = 0;
+let count = 0;
+let stack = []
+
+for (let i = 0; i < n; i++) {
+    if (stack.length === 0) {
+        stack.push(arr[i])
+        count++
+        continue;
+    }
+
+    const top = stack[stack.length - 1]
+    if (top === arr[i]) {
+        stack.push(arr[i])
+        count++
+        continue;
+    } else {
+        stack = []
+        stack.push(arr[i])
+        max = max > count ? max : count
+        count = 1
+    }
+}
+
+console.log(max)
