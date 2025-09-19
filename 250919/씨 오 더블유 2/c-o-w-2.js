a4c4570f698b20d05526b45127a3f65e354169ed
SK <54721624+ygvbhy@users.noreply.github.com>
2025-09-19 21:05:20 +0900

    Title: 씨 오 더블유 2, Time: 65ms, Memory: 10MB, Status: Passed - Codetree

--- 250919/씨 오 더블유 2/c-o-w-2.js
+++ 250919/씨 오 더블유 2/c-o-w-2.js
@@ -5,15 +5,11 @@ const n = Number(input[0]);
 const string = input[1];
 
 // Please Write your code here.
-let count = 0;
-for(let i = 0; i < n; i++) {
-    if (string[i] !== 'C') continue;
-    for (let j = i + 1; j < n; j++) {
-        if (string[j] !== 'O') continue;
-        for (let k = j + 1; k < n; k++) {
-            if (string[k] === 'W') count++
-        }
-    }
+let c = 0, co = 0, cow = 0
+for (let s of string) {
+    if (s === 'C') c += 1
+    else if (s === 'O') co += c
+    else if (s === 'W') cow += co
 }
 
-console.log(count)
+console.log(cow)
