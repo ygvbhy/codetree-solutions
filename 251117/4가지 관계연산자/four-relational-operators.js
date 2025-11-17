7ae44002378d4ba8efef8aac5d3e9956fab611c3
SK <54721624+ygvbhy@users.noreply.github.com>
2025-11-17 23:37:28 +0900

    Title: 4가지 관계연산자, Time: 63ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251117/4가지 관계연산자/four-relational-operators.js
@@ -0,0 +1,7 @@
+const fs = require("fs");
+let [a, b] = fs.readFileSync(0).toString().split(' ').map(Number)
+
+console.log(a >= b ? 1 : 0);
+console.log(a > b ? 1 : 0);
+console.log(b >= a ? 1 : 0);
+console.log(b > a ? 1 : 0);
