3d3fe7e5329378362b3fe8dcf0ae8f97589704d6
SK <54721624+ygvbhy@users.noreply.github.com>
2025-11-25 22:23:34 +0900

    Title: 장학금, Time: 64ms, Memory: 10MB, Status: Passed - Codetree

--- 251125/장학금/scholarship.js
+++ 251125/장학금/scholarship.js
@@ -4,5 +4,6 @@ let [m, f] = fs.readFileSync(0).toString().split(" ").map(Number);
 if (m >= 90) {
     if (f >= 95) console.log(100000)
     else if (f >= 90) console.log(50000)
+    else console.log(0)
 }
 else console.log(0)
