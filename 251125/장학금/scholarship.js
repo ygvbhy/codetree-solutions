e66ab853e1c9bb456667b6abe3547c6d1faa4367
SK <54721624+ygvbhy@users.noreply.github.com>
2025-11-25 22:22:30 +0900

    Title: 장학금, Time: 64ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 251125/장학금/scholarship.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+let [m, f] = fs.readFileSync(0).toString().split(" ").map(Number);
+
+if (m >= 90) {
+    if (f >= 95) console.log(100000)
+    else if (f >= 90) console.log(50000)
+}
+else console.log(0)
