4d1f8d50b5cfa6a723fb195a21a1768338595bf6
SK <54721624+ygvbhy@users.noreply.github.com>
2025-12-10 22:46:58 +0900

    Title: B부터 A까지 감소, Time: 69ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251210/B부터 A까지 감소/dec-from-b-to-a.js
@@ -0,0 +1,7 @@
+const fs = require("fs");
+let [a, b] = fs.readFileSync(0).toString().trim().split(' ').map(Number);
+
+let str = ''
+for (let i = b; i > a - 1; i--) str += i + ' '
+
+console.log(str.trim())
