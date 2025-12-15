f3c4bd6edd8ed6651199fddf1de00162b5b17de6
SK <54721624+ygvbhy@users.noreply.github.com>
2025-12-15 22:48:34 +0900

    Title: 높은 수에서 낮은 수까지, Time: 62ms, Memory: 10MB, Status: Passed - Codetree

--- 251215/높은 수에서 낮은 수까지/from-high-to-low.js
+++ 251215/높은 수에서 낮은 수까지/from-high-to-low.js
@@ -1,10 +1,13 @@
 const fs = require("fs");
 let [a, b] = fs.readFileSync(0).toString().trim().split(' ').map(Number);
 
-let str = ''
+let str = '';
 
-for (let i = b; i >= a; i--) {
-    str += i + ' '
+let min = Math.min(a, b);
+let max = Math.max(a, b);
+
+for (let i = max; i >= min; i--) {
+    str += i + ' ';
 }
 
-console.log(str.trim())
+console.log(str.trim());
