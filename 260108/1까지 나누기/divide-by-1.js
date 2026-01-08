25694a68ca92d7df5c9242e1d9f59818e027b234
SK <54721624+ygvbhy@users.noreply.github.com>
2026-01-08 22:42:39 +0900

    Title: 1까지 나누기, Time: 67ms, Memory: 10MB, Status: Passed - Codetree

--- 260108/1까지 나누기/divide-by-1.js
+++ 260108/1까지 나누기/divide-by-1.js
@@ -2,10 +2,11 @@ const fs = require("fs");
 let N = fs.readFileSync(0).toString().trim();
 
 let sum = +N;
-let i;
-for (i = 1; i <= N; i++) {
-    sum /= i
+let count = 0;
+for (let i = 1; i <= N; i++) {
+    sum = Math.floor(sum / i);
+    count++;
     if (sum <= 1) break;
 }
 
-console.log(i)
+console.log(count)
