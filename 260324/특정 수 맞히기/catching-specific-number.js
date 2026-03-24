b496306e0de1379fee386210cebd7dc6db145592
SK <54721624+ygvbhy@users.noreply.github.com>
2026-03-24 21:51:45 +0900

    Title: 특정 수 맞히기, Time: 69ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 260324/특정 수 맞히기/catching-specific-number.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split("\n");
+
+for (let i = 0; i < input.length; i++) {
+    if (input[i] === '25') {
+        console.log("Good");
+        break;
+    } else if (input[i] < '25') console.log("Higher");
+    else if (input[i] > '25') console.log("Lower")
+}
