a3abde68dede457230598bc61a3c0b0b70f99458
SK <54721624+ygvbhy@users.noreply.github.com>
2026-03-24 21:53:47 +0900

    Title: 특정 수 맞히기, Time: 65ms, Memory: 10MB, Status: Passed - Codetree

--- 260324/특정 수 맞히기/catching-specific-number.js
+++ 260324/특정 수 맞히기/catching-specific-number.js
@@ -1,10 +1,10 @@
 const fs = require("fs");
-let input = fs.readFileSync(0).toString().trim().split("\n");
+let input = fs.readFileSync(0).toString().trim().split("\n").map(Number);
 
 for (let i = 0; i < input.length; i++) {
-    if (input[i] === '25') {
+    if (input[i] === 25) {
         console.log("Good");
         break;
-    } else if (input[i] < '25') console.log("Higher");
-    else if (input[i] > '25') console.log("Lower")
+    } else if (input[i] < 25) console.log("Higher");
+    else if (input[i] > 25) console.log("Lower")
 }
