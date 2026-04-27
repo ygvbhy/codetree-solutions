33a950776f040a6aa5ad5bfbce85b97029402a0e
SK <54721624+ygvbhy@users.noreply.github.com>
2026-04-27 20:43:01 +0900

    Title: 직각 삼각형 출력, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260427/직각 삼각형 출력/Right-triangle-and-output.ts
@@ -0,0 +1,7 @@
+declare const require: any;
+const fs = require("fs");
+const N = +fs.readFileSync(0).toString().trim();
+
+for (let i = 1; i <= N; i++) {
+    console.log("*".repeat(i * 2 - 1));
+}
